d56559a545fc435d84ce76ca05a4c6cf7936c457
tsutsumi-ozro <soccerbakadaiki@icloud.com>
2023-03-26 17:58:08 +0900

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
-x = 10
-'x: ', x
+import streamlit as st
+import pandas as pd
+upload_file = st.file_uploader('アクセスログをアップロードしてください。')
+
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    st.markdown('### アクセスログ(先頭5件)')
+    st.write(df.head())
